fd84cb13489cc03c67750f978d243803b59d6e18
isabela <isabela2208aquino@gmail.com>
2025-03-05 13:39:44 -0300

    001

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "hiiii";
+});
+
+Route::get('/about', function () {
+    echo "about us";
 });
